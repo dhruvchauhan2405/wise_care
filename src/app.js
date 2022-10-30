6103cab6932f27e553e971d2d0fc21ac13bb53dc
Dhruv <dhruvsingh2405@gmail.com>
2022-10-30 23:36:33 +0530

    hee

--- src/app.js
+++ src/app.js
@@ -1,88 +1,46 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-const hbs = require("hbs");
+import React from "react";
+import { BrowserRouter as Router, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Homepages } from "./pages/Homepage/Homepages";
+import { Signin } from "./pages/SignIn-SignUp/Signin";
+import { SignUp } from "./pages/SignIn-SignUp/SignUp";
+import { BookApointment } from "./pages/BookAppointment/BookApointment";
+import { Yoga } from "./pages/Yogapage/Yoga";
+import { Exercise } from "./pages/Exercises/Exercise";
+import { Pregnant } from "./pages/Pregnant/Pregnant";
+import { MyFamily } from "./pages/MyFamily/MyFamily";
+import { Tests } from "./pages/Tests/Tests";
+import { DietPlans } from "./pages/DietPlans/DietPlans";
+import { HealthBolgs } from "./pages/HealthBlogs/HealthBolgs";
+import { Meditation } from "./pages/Meditation/Meditation";
+import { MedsMart } from "./pages/MedsMart/MedsMart";
 
-require("./db/conn");
-const Register = require("./models/registers");
-const { json } = require("express");
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepages />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/bookappointment" element={<BookApointment />} />
+          <Route path="/yoga" element={<Yoga />} />
+          <Route path="/exercise" element={<Exercise />} />
+          <Route path="/pregnant" element={<Pregnant />} />
+          <Route path="/myfamily" element={<MyFamily />} />
+          <Route path="/tests" element={<Tests />} />
+          <Route path="/dietplans" element={<DietPlans />} />
+          <Route path="/healthblogs" element={<HealthBolgs />} />
+          <Route path="/meditation" element={<Meditation />} />
+          <Route path="/medsmart" element={<MedsMart />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
 
-const port = process.env.PORT || 3000;
-
-const static_path = path.join(__dirname,"../public");
-const template_path = path.join(__dirname,"../templates/views");
-const partial_path = path.join(__dirname,"../templates/partials");
-
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-
-app.use(express.static(static_path));
-app.set("view engine","hbs");
-app.set("views",template_path);
-hbs.registerPartials(partial_path);
-
-app.get("/",(req,res)=>{
-    res.render("index")
-});
-
-app.get("/register",(req,res) => {
-    res.render("register");
-})
-
-app.get("/login",(req,res) => {
-    res.render("login");
-})
-//new user in our database
-app.post("/register", async(req,res) => {
-    
-    try{
-        const password = req.body.password;
-        const cpassword = req.body.confirmpassword;
-        
-        if (password === cpassword){
-            const registerEmployee = new Register({
-                firtsname: req.body.firstname,
-                lastname: req.body.lastname,
-                email:req.body.email,
-                gender: req.body.gender,
-                phone: req.body.phone,
-                age: req.body.age,
-                password: password,
-                confirmpassword:cpassword
-            })
-
-            //password hashing
-            middleware
-
-           const registered = await registerEmployee.save();
-           res.status(201).render("index");
-        }else{
-            res.send("password are not matching")
-        }
-    }catch(error){
-        res.status(400).send(error);
-    }
-})
-
-//for login
-
-app.post("/login",async(req,res) => {
-    try{
-        const email = req.body.email;
-        const password = req.body.password;
-
-        const useremail = await Register.findOne({email:email});
-        if(useremail.password === password){
-            res.status(201).render("index");
-        }else{
-            res.send("Invalid login details");
-        }
-
-    }catch(error) {
-        res.status(400).send("invalid Email")
-    }
-})
-
-app.listen(port,() => {
-    console.log(`server is running at port number ${port}`);
-})
+export default App;
