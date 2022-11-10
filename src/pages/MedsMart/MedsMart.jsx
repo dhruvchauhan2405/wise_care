bbd63700c46cfbd14b404eb7cb289eef80bfedba
Dhruv <dhruvsingh2405@gmail.com>
2022-11-10 20:36:37 +0530

    delete

--- src/pages/MedsMart/MedsMart.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const MedsMart = () => {
-  return <div>MedsMart</div>;
-};
--- src/pages/MyFamily/MyFamily.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const MyFamily = () => {
-  return <div>Family</div>;
-};
